6eefe4d78b79803466a7e3e6804a316f12cd2bf9
iamkevingreen <kevin@thecouch.nyc>
2019-05-19 19:04:42 -0700

    add the phone to the signup

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -17,12 +17,13 @@ function Signup() {
           e.preventDefault()
           const email = e.target.email.value
           const publication = e.target.publication.value
+          const phone = e.target.phone.value
           const password = e.target.password.value
           const mailChimpUrl = `https://a24films.us14.list-manage.com/subscribe/post?u=d6a612d44078d0634d5fa0663&amp;id=2b211ff970`
           load(signupUser(email, password))
             .then(user => {
               console.log("Success! Signed Up", user)
-              astrochimp(mailChimpUrl, { EMAIL: email, PUBLICA: publication }, (err, data) => {
+              astrochimp(mailChimpUrl, { EMAIL: email, PUBLICA: publication, PHONE: phone }, (err, data) => {
                 if (err) {
                   console.log(err)
                 } else {
@@ -37,17 +38,27 @@ function Signup() {
         <div>
           <h3>Sign Up</h3>
           <label>
-            <input className='auth__input x p1 mb1' type="email" name="email" placeholder='Email' />
+            <input className='auth__input x p1 mb1' required type="email" name="email" placeholder='Email' />
           </label>
         </div>
-        <div className='mb1 pb1'>
+        <div className='mb1'>
           <label>
-            <input className='auth__input x p1 mb1' type="password" name="password" placeholder='Password' />
+            <input className='auth__input x p1 mb1' required type="password" name="password" placeholder='Password' />
           </label>
         </div>
-        <div className='mb1 pb1'>
+        <div className='my05'>
+          <span class='akz-e caps small mb1 inline-block'>Optional</span>
+        </div>
+        <div className=''>
           <label>
             <input className='auth__input x p1 mb1' type="text" name="publication" placeholder='Publication' />
+          </label>
+        </div>
+        <div className='mb1 pb1'>
+          <label>
+            <input className='auth__input x p1 mb1' type="tel" id="phone" name="phone"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              placeholder='Phone Number' />
           </label>
         </div>
         <div>
