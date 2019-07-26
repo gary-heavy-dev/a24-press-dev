666d2438a862db490748597dcb8007bbf3b7c247
iamkevingreen <kevin@thecouch.nyc>
2019-07-26 19:18:39 -0400

    update to film row

--- src/components/filmRow.js
+++ src/components/filmRow.js
@@ -7,7 +7,8 @@ class FilmRow extends React.Component {
     const {
       title,
       slug,
-      releaseDate
+      releaseDate,
+      previewDate
     } = this.props.content
     const date = spacetime(releaseDate)
     return (
@@ -18,7 +19,10 @@ class FilmRow extends React.Component {
           <svg fill='currentColor' className='ml1' version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 55 20"><polygon fillRule='evenodd' clipRule='evenodd' points="43.3,0 55,10 43.3,20 42.4,19.1 52.2,10.7 0,10.7 0,9.3 52.2,9.3   42.4,0.9 43.3,0 " /></svg>
         </div>
         <div className='mr05'>
-          <h4 className='small caps m0 p0 akz-e'>{date.dayName()} {date.monthName()} {date.date()} {date.year()}</h4>
+          {!previewDate
+            ? (<h4 className='small caps m0 p0 akz-e'>{date.dayName()} {date.monthName()} {date.date()} {date.year()}</h4>)
+            : (<h4 className='small caps m0 p0 akz-e'>{previewDate}</h4>)
+          }
         </div>
       </div>
     )
